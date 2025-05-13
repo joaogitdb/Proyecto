547ff56121f81698821bf3241d7ae170bb2125bc
joaogitdb <joao-paulo.realista.ext@inetum.com>
2025-05-13 08:12:51 +0200

    img del deseño de la base de datos

--- gestor-reservas/src/main/java/com/empresa/inetum/gestor_reservas/repository/UsuarioRepository.java
+++ gestor-reservas/src/main/java/com/empresa/inetum/gestor_reservas/repository/UsuarioRepository.java
@@ -1,5 +1,11 @@
 package com.empresa.inetum.gestor_reservas.repository;
 
-public interface UsuarioRepository {
+import com.empresa.inetum.gestor_reservas.model.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Usuario findByUsername(String username);
 }
